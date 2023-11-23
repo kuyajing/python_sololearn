b8e151b2754926107d24b97e468b82a603aa66eb
kuyajing_np <kuya.jing.2011@gmail.com>
2023-11-23 13:18:15 +0800

    Changes to be committed: 	new file:   tuples_search.py

---
+++ tuples_search.py
@@ -0,0 +1,44 @@
+'''
+You are given a list of contacts, where each contact is represented by a tuple, with the name and age of the contact.
+Complete the program to get a string as input, search for the name in the list of contacts and output the age of the contact in the format presented below:
+
+Sample Input
+John
+
+Sample Output
+John is 31
+
+contacts = [
+    ('James', 42),
+    ('Amy', 24),
+    ('John', 31),
+    ('Amanda', 63),
+    ('Bob', 18)
+]
+
+'''
+
+
+def search_contact(contacts, name):
+    # Search for the name in the list of contacts
+    for contact in contacts:
+        if contact[0] == name:
+            # Output the age of the contact in the format presented below
+            return f"{name} is {contact[1]}"
+    # Return this message if the name is not found in the contacts list
+    return f"Not Found"
+
+# Sample list of contacts
+contacts = [
+    ('James', 42),
+    ('Amy', 24),
+    ('John', 31),
+    ('Amanda', 63),
+    ('Bob', 18)
+]
+
+# Get a string as input
+name = input()
+
+# Output the result
+print(search_contact(contacts, name))
